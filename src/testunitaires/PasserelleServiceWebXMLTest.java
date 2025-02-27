06b3e7fe62375f94802b6312b7db9c62381b5acd
Tom <tom.lefevrebonzon@gmail.com>
2025-02-27 13:49:57 +0100

    first commit

---
+++ src/testunitaires/PasserelleServiceWebXMLTest.java
@@ -0,0 +1,73 @@
+package testunitaires;
+
+import static org.junit.Assert.*;
+
+import java.text.ParseException;
+
+import org.junit.Test;
+
+import classes.Outils;
+import classes.PasserelleServicesWebXML;
+import classes.Point;
+import classes.PointDeTrace;
+import classes.Trace;
+import classes.Utilisateur;
+
+public class PasserelleServiceWebXMLTest {
+	
+	@Test
+	public void testConnecter() {
+		fail("Not yet implemented");
+	}
+		
+	@Test
+	public void testCreerUnUtilisateur() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSupprimerUnUtilisateur() {
+		fail("Not yet implemented");	
+	}
+	
+	@Test
+	public void testChangerDeMdp() {
+		fail("Not yet implemented");
+	}	
+
+	@Test
+	public void testDemanderMdp() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDemanderUneAutorisation() {
+		fail("Not yet implemented");	
+	}	
+	
+	@Test
+	public void testRetirerUneAutorisation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEnvoyerPosition() throws ParseException {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDemarrerEnregistrementParcours() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArreterEnregistrementParcours() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSupprimerUnUnParcours() {
+		fail("Not yet implemented");
+	}
+	
+} // fin du test
---
+++ src/testvisuels/TestPasserelleServicesWebXML.java
@@ -0,0 +1,33 @@
+package testvisuels;
+
+import java.text.ParseException;
+
+import classes.Outils;
+import classes.PasserelleServicesWebXML;
+import classes.PointDeTrace;
+import classes.Trace;
+import classes.Utilisateur;
+
+public class TestPasserelleServicesWebXML {
+
+	public static void main(String[] args) throws ParseException {
+		
+		String msg;
+	
+		// test visuel de la méthode getTousLesUtilisateurs
+
+
+		// test visuel de la méthode getLesUtilisateursQueJautorise
+
+
+		// test visuel de la méthode getLesUtilisateursQuiMautorisent
+
+		
+		// test visuel de la méthode getLesParcoursDunUtilisateur
+
+		
+		// test visuel de la méthode getUnParcoursEtSesPoints
+
+	
+	} // fin Main
+} // fin class
